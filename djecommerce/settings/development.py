e248ff088380301ed14f50a62f9280b5ba88f36c
luisagcenteno84 <luisagcenteno@gmail.com>
2024-12-30 20:16:02 -0700

    trying without src

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -39,7 +39,7 @@ DEBUG_TOOLBAR_CONFIG = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'src/db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '/db.sqlite3'),
     }
 }
 
